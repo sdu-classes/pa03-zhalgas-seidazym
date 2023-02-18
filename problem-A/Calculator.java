1cd8db72c9b8d801bf7488338b2b1e2a7f59a0e3
zhalgas-seidazym <124429967+zhalgas-seidazym@users.noreply.github.com>
2023-02-18 15:16:29 +0600

    deleted try catch in methods

--- problem-A/Calculator.java
+++ problem-A/Calculator.java
@@ -2,55 +2,30 @@ public class Calculator {
     private int num1;
     private int num2;
 
-//-------------Cinstructor------------
+//-------------Constructor------------
     public Calculator(double n1, double n2){
-        try{
-        if((int)n1 != n1 || (int)n2 != n2) throw new NumberFormatException();
+        if((int)n1 != n1 || (int)n2 != n2) throw new NumberFormatException("Your number is not int");
         else{ 
             this.num1 = (int)n1;
             this.num2 = (int)n2;
             }
-        }catch(NumberFormatException n){
-            n.printStackTrace();
-        }
     }
     
 //-------------------Methods---------------
     public int Add(){
-        try{
-            if(num1 < 0 || num2 < 0)throw new ArithmeticException();
-            else return num1 + num2;
-        }catch(ArithmeticException a){
-            a.printStackTrace();
-            return 1;
-        }
+        if(num1 < 0 || num2 < 0)throw new ArithmeticException(String.format("One of your number is negative: " + num1 + " or " + num2));
+        else return num1 + num2;
     }
     public int Substract(){
-        try{
-            if(num1 < 0 || num2 < 0)throw new ArithmeticException();
-            else return num1 - num2;
-        }catch(ArithmeticException a){
-            a.printStackTrace();
-            return 1;
-        }
+        if(num1 < 0 || num2 < 0)throw new ArithmeticException(String.format("One of your number is negative: " + num1 + " or " + num2));
+        else return num1 - num2;
     }
     public int Myltiply(){
-        try{
-            if(num1 == 0 || num2 == 0)throw new ArithmeticException();
-            else return num1 * num2;
-        }catch(ArithmeticException a){
-            a.printStackTrace();
-            return 1;
-        }
+        if(num1 == 0 || num2 == 0)throw new ArithmeticException(String.format("One of your number is zero: " + num1 + " or " + num2));
+        else return num1 * num2;
     }
     public int Divide(){
-        try{
-            if(num1 == 0 || num2 == 0)throw new ArithmeticException();
-            else return num1 / num2;
-        }catch(ArithmeticException a){
-            a.printStackTrace();
-            return 1;
-        }
+        if(num1 == 0 || num2 == 0)throw new ArithmeticException(String.format("One of your number is zero: " + num1 + " or " + num2));
+        else return num1 / num2;
     }
-
 }
